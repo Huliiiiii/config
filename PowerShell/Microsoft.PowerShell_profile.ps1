88443632c5c9825e766d43f2e304ae3b5227168f
Huli <134658521+Huliiiiii@users.noreply.github.com>
2025-06-01 04:18:27 +0800

    Update powershell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,40 @@ $_src = "$ConfigPath\Powershell\Src"
 
 Set-Alias sh "C:\Program Files\Git\git-bash.exe"
 
+function wezssh() {
+    param(
+        [Parameter(Position=0)]
+        [string]$target
+    )
+
+    Start-Process wezterm -WindowStyle Hidden -ArgumentList "ssh $target"
+}
+
 ssh-add "$Home/.ssh/id_rsa" *> $null
+
+function dl {
+    param (
+        [string]$url
+        # [string[]]$ExtraArgs
+    )
+
+    if ([string]::IsNullOrWhiteSpace($url)) {
+        Write-Error "Error: URL cannot be empty."
+        return
+    }
+
+    do {
+        Write-Host "Trying to download $url..."
+        wget -c -t 0 --retry-connrefused --waitretry=5 @ExtraArgs $url 
+        $success = $LASTEXITCODE
+        if ($success -ne 0) {
+            Write-Host "Download failed. Retrying in 5 seconds..."
+            Start-Sleep -Seconds 5
+        }
+    } while ($success -ne 0)
+
+    Write-Host "Download completed successfully!"
+}
 
 [console]::OutputEncoding = [console]::InputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
 
@@ -21,16 +54,16 @@ ssh-add "$Home/.ssh/id_rsa" *> $null
         $global:documents = [System.Environment]::GetFolderPath('MyDocuments')
         $global:music = [System.Environment]::GetFolderPath('MyMusic')
     },
-    # {
-    #     Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-    #         param($wordToComplete, $commandAst, $cursorPosition)
-    #         $Local:word = $wordToComplete.Replace('"', '""')
-    #         $Local:ast = $commandAst.ToString().Replace('"', '""')
-    #         winget complete --word "$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-    #             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    #         }
-    #     }
-    # },
+    {
+        Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+            param($wordToComplete, $commandAst, $cursorPosition)
+            $Local:word = $wordToComplete.Replace('"', '""')
+            $Local:ast = $commandAst.ToString().Replace('"', '""')
+            winget complete --word "$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+        }
+    },
     (. "$_src/Aliases.ps1"),
     # PSReadline Settings
     (. "$_src/Function.ps1"),
@@ -38,5 +71,6 @@ ssh-add "$Home/.ssh/id_rsa" *> $null
     # bun auto complete
     (. "$_src/Completion/bun.ps1"),
     (Invoke-Expression  (&just --completions powershell | Out-String)),
-    (Invoke-Expression "$(direnv hook pwsh)")
+    (Invoke-Expression "$(direnv hook pwsh)"),
+    {}
 )  | ForEach-Object { Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -MaxTriggerCount 1 -Action $_ }| Out-Null
